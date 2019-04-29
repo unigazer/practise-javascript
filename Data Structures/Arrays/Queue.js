0682f34225e16248b944d197efb439a1835f57fb
Vladimir Jovanović <vladimir.jovanovic993@gmail.com>
2019-04-29 16:27:23 +0200

    Stacks and Queue (Data Structure)

---
+++ Data Structures/Arrays/Queue.js
@@ -0,0 +1,13 @@
+/* --- Queue --- */
+
+// Declare empty queue array
+let queue = [];
+
+// Add the element to the queue
+queue.push(1);
+
+// Append new element to the queue
+queue.push(2);
+
+// Remove the first element from the queue
+queue.shift();
---
+++ Data Structures/Arrays/Stacks.js
@@ -0,0 +1,13 @@
+/* --- Stacks --- */
+
+// Declare empty stack array
+let stack = [];
+
+// Add the element to stack
+stack.push(1);
+
+// Append a new element to the existing stack
+stack.push(2);
+
+// Remove the last element from the stack
+stack.pop();
